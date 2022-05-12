c89e9ca7e107eab010d8849e2d03608cf3d74271
Samantha Crespo <samkcrespo@gmail.com>
2022-05-12 15:24:57 -0700

    single recipe styling update

--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function SingleRecipe() {
@@ -9,56 +9,65 @@ function SingleRecipe() {
 
   useEffect(() => {
     fetch(`http://localhost:4000/recipes/${id}`)
-    .then(res => res.json())
-    .then(data => {
-      setSingleRecipe(data);
-      setIsLoaded(true);
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSingleRecipe(data);
+        setIsLoaded(true);
+      });
   }, [id]);
 
-
   // const selectedRecipe = recipeData.filter(recipe => recipe.id === JSON.parse(id))
-   
+
   // console.log(selectedRecipe)
 
   // const styles = {
-  //   borderTop: '5px solid rgba(0, 0, 0, 0.05)', 
+  //   borderTop: '5px solid rgba(0, 0, 0, 0.05)',
   // }
 
   if (!isLoaded) return <h2>Loading...</h2>;
-  
-return (
-  <div key={singleRecipe.id} className="row">
-    <div  className="column" >
-    <div className="paddingClass">
-    <img src={singleRecipe.image} alt={singleRecipe.name} height="400"  width= "400"/>
+
+  return (
+    <div key={singleRecipe.id} className="row">
+      <div className="column">
+        <div className="paddingClass">
+          <div className="imageDiv">
+            <img
+              className="singleRecipeImg"
+              src={singleRecipe.image}
+              alt={singleRecipe.name}
+              height="400"
+              width="400"
+            />
+          </div>
+        </div>
       </div>
-      </div>
-    <div className="column">
-    
-    <h3 className = "recipeName"> {singleRecipe.name}</h3>
-    <h3>Ingredients:</h3>
-    <ul> {singleRecipe.ingredients.map((ingredient,idx) =>
-      <li key={idx}>{ingredient}</li>
-      )}
-    </ul>
-    <h3>Instructions:</h3> 
-      {/* {
+      <div className="column">
+        <h3 className="recipeName"> {singleRecipe.name}</h3>
+        <h3>Ingredients:</h3>
+        <ul>
+          {" "}
+          {singleRecipe.ingredients.map((ingredient, idx) => (
+            <li key={idx}>{ingredient}</li>
+          ))}
+        </ul>
+        <h3>Instructions:</h3>
+        {/* {
       Object.entries(selectedRecipe[0].instructions).map(([key, val]) => 
             <h4 key={key}>{key}:{val}</h4>  
         )
       } */}
-      {singleRecipe.instructions.map((recipe, i) => {
-        return (
-          <div>
-          <h4 key={i}>{Object.keys(recipe)}: {Object.values(recipe)}</h4>
-          </div>
-          )
-      })}
+        {singleRecipe.instructions.map((recipe, i) => {
+          return (
+            <div>
+              <h4 key={i}>
+                {Object.keys(recipe)}: {Object.values(recipe)}
+              </h4>
+            </div>
+          );
+        })}
       </div>
-  </div> 
-
-)
+    </div>
+  );
 }
 
-export default SingleRecipe
+export default SingleRecipe;
